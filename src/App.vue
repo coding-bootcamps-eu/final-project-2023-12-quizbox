f999ac37ac9bbbab0935aaddbc8f309f9a6bb15d
Philipp Theilacker <philipp.theilacker@gmx.de>
2024-03-12 16:07:38 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="hamburger-menu">
+        <span class="hamburger-menu__icon"></span>
+        <span class="hamburger-menu__icon"></span>
+        <span class="hamburger-menu__icon"></span>
       </nav>
+      <h1 class="">Quizbox</h1>
     </div>
+    <nav class="page-header__menu--secondary" :class="{ visible: showNav, hidden: !showNav }">
+      <div class="nav">
+        <ul class="nav__list">
+          <li class="nav__item">
+            <router-link
+              :to="{ name: 'quizbox' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
+              >Play Quizbox</router-link
+            >
+          </li>
+          <li class="nav__item">
+            <router-link
+              :to="{ name: 'questions' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
+              >Browse Questions</router-link
+            >
+          </li>
+          <li class="nav__item" v-if="sessionActive">
+            <router-link
+              :to="{ name: 'session' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
+              >Resume Session</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
